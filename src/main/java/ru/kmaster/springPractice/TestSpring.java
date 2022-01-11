34a8295fe8403e9b63c7f46df002a4cd147f8ea5
Krylov <keriesa13@gmail.com>
2022-01-11 22:06:11 +0400

    Внедрение зависимостей отдельным файлом

--- src/main/java/ru/kmaster/springPractice/TestSpring.java
+++ src/main/java/ru/kmaster/springPractice/TestSpring.java
@@ -4,9 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
