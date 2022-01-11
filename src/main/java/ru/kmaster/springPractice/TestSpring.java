7550791950e1fafc44153d7eed69facf7f794d81
Krylov <keriesa13@gmail.com>
2022-01-11 23:11:32 +0400

    Области видимости бинов(в прошлых вариантах был синглтон, сейчас прототайп)

--- src/main/java/ru/kmaster/springPractice/TestSpring.java
+++ src/main/java/ru/kmaster/springPractice/TestSpring.java
@@ -5,12 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
+        System.out.println(firstMusicPlayer == secondMusicPlayer);
 
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getName());
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
