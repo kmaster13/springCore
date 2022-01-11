3c42d324bfd4bf80ad51fa32f19c2c4989dc6463
Krylov <keriesa13@gmail.com>
2022-01-12 00:24:59 +0400

    Простая реализация init destroy factory методов

--- src/main/java/ru/kmaster/springPractice/ClassicalMusic.java
+++ src/main/java/ru/kmaster/springPractice/ClassicalMusic.java
@@ -1,6 +1,19 @@
 package ru.kmaster.springPractice;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return  new ClassicalMusic();
+    }
+
+    private void doInitMethod() {
+        System.out.println("Doing my initialization");
+    }
+
+    private void doDestroyMethod() {
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
--- src/main/java/ru/kmaster/springPractice/TestSpring.java
+++ src/main/java/ru/kmaster/springPractice/TestSpring.java
@@ -5,8 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+/*        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         System.out.println(firstMusicPlayer == secondMusicPlayer);
 
@@ -16,7 +18,7 @@ public class TestSpring {
         firstMusicPlayer.setVolume(10);
 
         System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());*/
 
         context.close();
     }
