59364f04951aca2ee50783bf4c5e384305efbbdb
Krylov <keriesa13@gmail.com>
2022-01-12 09:12:21 +0400

    Пример аннотаций @PostConstruct, @PreDestroy (в 11 java были удалены, но есть репозиторий для maven javax.annotation-api)

--- src/main/java/ru/kmaster/springPractice/ClassicalMusic.java
+++ src/main/java/ru/kmaster/springPractice/ClassicalMusic.java
@@ -1,11 +1,24 @@
 package ru.kmaster.springPractice;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("singleton")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
+
     @Override
     public String getSong() {
         return "Турецкий марш";
--- src/main/java/ru/kmaster/springPractice/TestSpring.java
+++ src/main/java/ru/kmaster/springPractice/TestSpring.java
@@ -6,17 +6,9 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
 
-        //scope = singleton
-        System.out.println(classicalMusic1 == classicalMusic2);
-
-        RapMusic rapMusic1 = context.getBean("rapMusic", RapMusic.class);
-        RapMusic rapMusic2 = context.getBean("rapMusic", RapMusic.class);
-
-        //scope = prototype
-        System.out.println(rapMusic1 == rapMusic2);
+        System.out.println(classicalMusic.getSong());
 
         context.close();
     }
